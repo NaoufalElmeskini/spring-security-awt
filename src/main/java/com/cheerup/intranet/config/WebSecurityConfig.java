f45661e177ce1aae9efbb33cea05b0dcec451b36
nacrobate <naoufal.elmeskini@gmail.com>
2021-10-21 18:04:07 +0200

    migration springfox -> springdoc done

--- src/main/java/com/cheerup/intranet/config/WebSecurityConfig.java
+++ src/main/java/com/cheerup/intranet/config/WebSecurityConfig.java
@@ -6,22 +6,16 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
+
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private static final String[] AUTH_WHITELIST = {
             "/swagger-resources/**",
             "/swagger-ui/*",
-            "/swagger-ui.html",
-            "/v2/api-docs",
+            "/v3/api-docs",
             "/webjars/**"
     };
 
@@ -35,7 +29,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/login", "/register", "/swagger-ui/*").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
--- src/main/java/com/cheerup/intranet/controller/AuthenticationController.java
+++ src/main/java/com/cheerup/intranet/controller/AuthenticationController.java
@@ -2,6 +2,8 @@ package com.cheerup.intranet.controller;
 
 import com.cheerup.intranet.model.AuthRequest;
 import com.cheerup.intranet.util.JwtUtil;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,8 +24,9 @@ public class AuthenticationController {
     @Autowired
     private JwtUtil jwtUtil;
 
-    @GetMapping(path = "/hello")
-    public ResponseEntity<String> hello() {
+    @GetMapping(path = "/userdetail")
+    @Operation(summary = "User details endpoint", security = @SecurityRequirement(name = "bearerAuth"))
+    public ResponseEntity<String> getUserDetails() {
         return ResponseEntity.ok()
                 .body("Bienvenue dans Cheer-up Intranet !");
     }
